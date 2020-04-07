0c0a2145608f4814755224c9e91b96639016d7eb
Rance Campbell <rancewcampbell@gmail.com>
2020-04-07 22:15:42 +0000

    added interactive timer

--- timer1.js
+++ timer1.js
@@ -1,6 +1,5 @@
 const timer = () => {
   const alarms = process.argv.slice(2).filter((el) => (el >= 0 && +el));
-  console.log(alarms);
   for (const alarm of alarms) {
     setTimeout(() => {
       process.stdout.write('BEEEEEEEEP!\n');
--- timer2.js
+++ timer2.js
@@ -5,17 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const timer = () => {
-  const alarms = process.argv.slice(2).filter((el) => (el >= 0 && +el));
-  console.log(alarms);
-  for (const alarm of alarms) {
-    setTimeout(() => {
-      process.stdout.write('BEEEEEEEEP!\n');
-    }, alarm * 1000);
-  }
-};
-
 rl.question('Set alarm for how long? ', (time) => {
-  console.log(time);
-  rl.close();
+  console.log(`Setting a timer for ${time} seconds...`);
+  
+  const alarm = setTimeout(() => {
+    console.log('\nBEEEEEEEEP!\n');
+    rl.close();
+  }, time * 1000);
+  
+
+  rl.on('close', () => {
+    console.log('Thanks for using me, ciao!');
+    clearTimeout(alarm);
+    rl.close();
+  });
+
 });
